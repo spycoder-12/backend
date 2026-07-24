ed469db9b297f8850fdc143f4353f744c06e2052
Nitin Verma <nverrma123@gmail.com>
2026-07-25 01:19:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,7 +67,7 @@ SMTP_USER = os.getenv("SMTP_USER", "nverrma123@gmail.com")
 SMTP_PASS = os.getenv("SMTP_PASS", "xrksxzwpeperpgue")
 NOTIFY_EMAIL = os.getenv("NOTIFY_EMAIL", "nverrma123@gmail.com")
 
-RESEND_API_KEY = os.getenv("RESEND_API_KEY", "re_dpWw8yJV_4ogKQ1Ut1SgXU6bAiiRipDrm")
+RESEND_API_KEY = os.getenv("RESEND_API_KEY", "re_A4pbkddM_PrChJhoMXybyk8Nq2UqHvHGw")
 
 
 # TWILIO_SID = os.getenv("ACa3bc4b2c803385dafd623f8fdb8864f2")
